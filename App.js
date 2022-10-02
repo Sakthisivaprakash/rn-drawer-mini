ed499ccd0aa7065ecb9f7215f66feeca26ecaa00
Sakthisivaprakash <sakthisivaprakash@gmail.com>
2022-10-02 22:10:15 +0530

    options updated for drawer screeb styling

--- App.js
+++ App.js
@@ -2,18 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import "react-native-gesture-handler";
 
-import WelcomeScreen from './screens/WelcomeScreen';
-import UserScreen from './screens/UserScreen';
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-       <Drawer.Navigator initialRouteName="User">
-          <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-          <Drawer.Screen name="User" component={UserScreen} />
-       </Drawer.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "#fff",
+            drawerLabel: "Welcome Screen",
+            drawerActiveBackgroundColor: "#f0e1ff",
+            drawerActiveTintColor: "#3c0a6b",
+            drawerStyle: { backgroundColor: "#ccc" },
+          }}
+        />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
